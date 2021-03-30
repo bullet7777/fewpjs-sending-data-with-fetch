3382222ca1e28f73e2c2a8e576a5ac34be2e9cd1
chris <chris.mahfouz@hotmail.com>
2021-03-30 11:34:34 +0300

    completed assignment data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+function submitData(userName,userEmail){
+
+let newData={
+    name:userName,
+    email:userEmail
+}
+
+
+let configObject={
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+    },
+    body:JSON.stringify(newData)
+}
+
+
+
+
+return fetch("http://localhost:3000/users",configObject)
+.then(function(response){
+    return response.json();
+})
+.then(function(object){
+    document.body.innerHTML = object.id
+})
+.catch(function(error) {
+   document.body.innerHTML=error.message
+  
+  }); 
+
+
+
+}
